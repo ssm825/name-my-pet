d553db7d1bc0972f21b5b9d2d91251d3f8ecce53
ssm825 <ssm39825@gmail.com>
2023-06-14 19:37:07 +0900

    Feat : OpenAI api fetch

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,28 @@ export default function Home() {
     setInput(e.target.value);
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    setInput("");
+
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ pet: input }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.staus}`)
+        );
+      }
+      setInput("");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
   };
 
   return (
