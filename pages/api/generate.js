223e37170edd776b9d396a5f14c625753c852c73
ssm825 <ssm39825@gmail.com>
2023-06-15 23:33:48 +0900

    Feat : try-catch로 예외 처리

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,13 +19,36 @@ export default async function (req, res) {
   }
 
   const pet = req.body.pet || "";
+  if (pet.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "반려동물을 입력해 주세요!",
+      },
+    });
+    return;
+  }
 
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `suggest three pet names for the follow ${pet}`,
-    temperature: 0.8,
-    max_tokens: 100,
-  });
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `suggest three pet names for the follow ${pet}`,
+      temperature: 0.8,
+      max_tokens: 100,
+    });
 
-  res.status(200).json({ result: response.data.choices[0].text });
+    res.status(200).json({ result: response.data.choices[0].text });
+  } catch (error) {
+    const { status, data } = error.response;
+    if (error.response) {
+      console.error(status, data);
+      res.stauts(status).json(data);
+    } else {
+      console.error(`OpenAI API 요청 오류 발생 : ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "요청 중 오류 발생",
+        },
+      });
+    }
+  }
 }
