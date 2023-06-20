49ad41b24be9653350239660bb908e7eec0e2b69
ssm825 <ssm39825@gmail.com>
2023-06-20 23:34:34 +0900

    Feat : useForm 커스텀 훅 추가 - 유효성 검사 및 에러 상태 관리

---
+++ pages/hooks/useForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const useForm = (initialValue) => {
+  const [inputValue, setInputValue] = useState(initialValue);
+  const [text, setText] = useState(initialValue);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
+    setError("");
+  };
+
+  const handleText = (e) => {
+    setText(e.target.textContent);
+    setError("");
+  };
+
+  const validate = (validateFunc, errorMessage) => {
+    if (!validateFunc(inputValue || text)) {
+      setError(errorMessage);
+      return false;
+    }
+    return true;
+  };
+
+  return {
+    inputValue,
+    setInputValue,
+    text,
+    handleText,
+    error,
+    handleChange,
+    validate,
+  };
+};
+
+export default useForm;
