da19e8b185e51971ff7456cd2fb5f7381b2286e1
ssm825 <ssm39825@gmail.com>
2023-06-22 23:27:25 +0900

    Chore : fontawesome 설치

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
