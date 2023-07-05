1af0d360d7006da5702bf86d5870d419ba3b6a3e
ssm825 <ssm39825@gmail.com>
2023-07-05 23:15:59 +0900

    Feat : ThemeProvider로 style을 전역적으로 공유

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { ThemeProvider } from "styled-components";
 config.autoAddCss = false;
 
-import "../styles/globals.css";
+import theme from "../styles/theme";
+import GlobalStyles from "../styles/globalStyle";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
+  );
 };
 
 export default MyApp;
