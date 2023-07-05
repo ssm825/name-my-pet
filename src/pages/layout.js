b8c052fa285eb106a53aef0fb8679359741112f2
ssm825 <ssm39825@gmail.com>
2023-07-05 23:02:24 +0900

    Feat : SSR에서 styled-components를 사용하기 위한 설정 및 파일 추가

---
+++ src/pages/layout.js
@@ -0,0 +1,11 @@
+import StyledComponentsRegistry from "../lib/registry";
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
